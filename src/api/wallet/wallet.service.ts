8f91d4c5fc69b536e8281af60964c95fc6219ef0
Seçkin Özdemir <seckin.ozdemir@getir.com>
2022-06-04 16:43:26 +0300

    Updated comment lines.

--- src/api/wallet/wallet.service.ts
+++ src/api/wallet/wallet.service.ts
@@ -10,7 +10,9 @@ export class WalletService {
   ) {}
 
   /*
-    belirli bir decimal point’ten sonra http Express vb sayıları string olarak client response olarak döner.
+    BigNumber type ının responseta string olarak görünmesi.
+
+    Belirli bir decimal point’ten sonra http Express vb sayıları string olarak client response olarak döner.
     Date timestamp de bu şekilde. Bunun olmaması için repsonse DTO yazılması ve dönüş tiplerinin özellikle
     belirtilmesi lazım bu number’dır, bu string’tir diye.
   */
